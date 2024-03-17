9b1ac8c363ba4fd2d8590f5ee4728779c8411e8e
sacm <sacm1046@gmail.com>
2024-03-17 19:03:44 -0300

    formateos varios

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react"
 
-export default function Home () {
+export default function Home() {
+    const [tasks, setTasks] = useState([])
 
-    const getUsers = async () => {
-        const response = await fetch("https://run.mocky.io/v3/41d1f823-da3a-45d6-8421-0c52d54dc4be");
+    const getTasks = async () => {
+        const response = await fetch("https://api-tasks.vercel.app/api/tasks")
         const data = await response.json()
+        setTasks(data)
+    }
+
+    const createTask = async () => {
+        const response = await fetch("https://api-tasks.vercel.app/api/task", {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                responsable: "Javier",
+                description: "Esta es una tarea de Javier"
+            })
+        })
+        const data = await response.json()
+        await getTasks()
         console.log(data)
     }
 
-    useEffect(()=> {
-        getUsers()
+    useEffect(() => {
+        getTasks()
     }, [])
 
     return <div>
         <h1>Home desde un componente</h1>
+        <button onClick={() => createTask()}>Crear tarea</button>
+        {
+            tasks.map((task) => {
+                return <div key={task.id}>
+                    <div>Responsable: {task.responsable}</div>
+                    <div>Descripción: {task.description}</div>
+                    <hr />
+                </div>
+            })
+        }
     </div>
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import MyRoutes from './Routes/index.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <BrowserRouter>
-      <MyRoutes></MyRoutes>
+      <MyRoutes />
     </BrowserRouter>
-  </React.StrictMode>,
 )
